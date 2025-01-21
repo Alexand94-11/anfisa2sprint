333ef5986ebc79d4dd1e4efff5773449a6c82365
Александр Пак <pack.alexander@yandex.ru>
2025-01-21 03:13:57 +0300

    pep 8 changes

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добавляет флаг is_published."""
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(default=True,
+                                       verbose_name='Опубликовано')
 
     class Meta:
         abstract = True
