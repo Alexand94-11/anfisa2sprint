a494bca42910b77a0d11ebf90d225498b53fbffe
Александр Пак <pack.alexander@yandex.ru>
2025-01-22 04:47:24 +0300

    add sort

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -8,11 +8,13 @@ from ice_cream.models import IceCream
 def index(request):
     template_name = 'homepage/index.html'
     # Запрашиваем нужные поля моделей + используем фильтр + исключаем
-    # неопубликованные записи + используем Q-объекты:
+    # неопубликованные записи + используем Q-объекты + используется сортировка
+    # по алфавиту + выводятся первые 3 объекта:
     ice_cream_list = IceCream.objects.values(
-        'title', 'description'
+        'id', 'title', 'description'
         ).filter(Q(is_published=True) &
-                 (Q(is_on_main=True) | Q(title__contains='пломбир')))
+                 (Q(is_on_main=True) | Q(title__contains='пломбир'))
+                 ).order_by('title')[1:4]
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
